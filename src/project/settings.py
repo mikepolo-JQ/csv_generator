7b8e02f33081e36bfa67bee1de5bd1f8516df47d
Misha Polozkov <polozkov1450@gmail.com>
2021-04-05 21:27:33 +0300

    sentry

--- src/project/settings.py
+++ src/project/settings.py
@@ -22,6 +22,23 @@ ALLOWED_HOSTS = [
     _ds.HOST,
 ]
 
+if not DEBUG:
+    # sentry_sdk.init(_ds.SENTRY_DSN, traces_sample_rate=1.0)
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=_ds.SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
